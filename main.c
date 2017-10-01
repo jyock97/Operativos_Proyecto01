7ee1c3b0d7f55dd4f0fee63d9716f6006013ed66
Jose Paulo Yock Fuentes <jyock1997@gmail.com>
2017-10-01 17:03:05 -0600

    Se realiza un chat para probar conecciones

--- main.c
+++ main.c
@@ -11,15 +11,19 @@
 #include <errno.h>
 
 
-int sock, newSock;  //file descriptor del socket
+int sock, newSock, useSock;  //file descriptor del socket
 int clientLen;
-int port;             //numero del puerto
+int port = 5000;             //numero del puerto
 int charReadWriteSize;
 char *ipAddr;
 char buffer[256];
 
 struct sockaddr_in serv_addr, cli_addr; //direccion del servidor y el cliente
 struct hostent *server;
+
+int retval;
+fd_set fdList;
+struct timeval tv;
 
 void error(char *msg){
   perror(msg);
@@ -39,24 +43,24 @@ void initServer(){
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);   //direccion ip de la maquina
   serv_addr.sin_port = htons(port);
-  printf("asdf\n");
+
   bind(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
 
   listen(sock,2);     //listo para recibir conexiones
 
   clientLen = sizeof(cli_addr);
   newSock = accept(sock, (struct sockaddr*) &cli_addr, &clientLen);
-  printf("aaaa\n");
   if(newSock < 0)
     error("ERROR on accept");
 
+  useSock = newSock;
 
-  //bzero(buffer, sizeof(buffer));
+  /*fflush(stdin);
   charReadWriteSize = read(newSock, buffer, sizeof(buffer));
   if(charReadWriteSize < 0)
     error("ERROR reading from socket");
 
-  printf("%s\n", buffer);
+  printf("%s\n", buffer);*/
 }
 
 void initClient(){
@@ -65,10 +69,9 @@ void initClient(){
   if (sock < 0)   //error creando el socket
     error("ERROR opening socket");
 
-  port = 5000;
-  ipAddr = "192.168.1.120";
+  printf("Ingrese el ip del servidor: ");
+  scanf("%s\n", ipAddr);
 
-  //bzero((char *) &serv_addr, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = inet_addr(ipAddr);
   serv_addr.sin_port = htons(port);
@@ -76,13 +79,14 @@ void initClient(){
   if (connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR connecting");
 
-  printf("Please enter the message: ");
+  useSock = sock;
+  /*printf("Please enter the message: ");
   //bzero(buffer,256);
   fgets(buffer,255,stdin);
   charReadWriteSize = write(sock,buffer,strlen(buffer));
 
   if (charReadWriteSize < 0)
-    error("ERROR writing to socket");
+    error("ERROR writing to socket");*/
 }
 
 int main(){
@@ -108,6 +112,27 @@ int main(){
     case 'C':
       initClient();
       break;
+  }
+
+  FD_ZERO(&fdList);
+  FD_SET(0, &fdList);
+  FD_SET(useSock, &fdList);
+  tv.tv_sec = 1;
+  tv.tv_usec = 0;
+  while (1) {
+    retval = select(FD_SETSIZE, &fdList, NULL, NULL, &tv);
+
+    if(retval == 1){
+      if(FD_ISSET (0, &fdList)){
+
+        fgets(buffer, sizeof(buffer), stdin);
+        write(useSock, buffer, sizeof(buffer));
+      }
+      if(FD_ISSET (useSock, &fdList)){
+        if(read(useSock, buffer, sizeof(buffer)) > 0)
+          printf("%s\n", buffer);
+      }
+    }
   }
 
   return 0;
