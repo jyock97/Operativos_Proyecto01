cdffb7360ac8d4b2fea7ed3607c73938af2fc1f7
Julio_R <j.rojas.c123@gmail.com>
2017-10-08 13:00:18 -0600

    threads listos

--- mythreads/test.c
+++ mythreads/test.c
@@ -1,12 +1,20 @@
 #include "scheduler.h"
 #include "my_thread.h"
+#include "my_mutex.h"
+
+my_mutex lock;
 
 void thread1(){
     int id = 0;
+
     while(1) {
+        if(!id){
+            my_mutex_lock(&lock);
+        }
         printf("hola %i\n", id++);
         sleep(1);
-        my_thread_end();
+        my_mutex_unlock(&lock);
+        //my_thread_end();
     };
 }
 
@@ -20,13 +28,17 @@ void thread3(){
 
 void thread2(){
     int id = 0;
+    my_mutex_lock(&lock);
     printf("hola1 %i\n", id++);
     my_thread_create(thread3,5,1);
+    my_mutex_unlock(&lock);
 }
 
 //archivo de pruebas para los hilos
 int main(){
     int i;
+
+    my_mutex_init(&lock);
     my_thread_create(thread1,5,2);
     my_thread_create(thread2,5,3);
     my_thread_chsched("Real");
